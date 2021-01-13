45f4fb71de1f23d1edb99f27d11dec684cfeaa9d
harrissonrb <harrissonrocha@hotmail.com>
2021-01-12 23:14:06 -0300

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -2,9 +2,13 @@ module.exports = {
 
   client: 'postgresql',
   connection: {
-    database: 'db',
-    user: 'postgres',
-    password: '123456'
+    database: 'd44arciics89r9',
+    Port: '5432',
+    user: 'd44arciics89r9',
+    password: 'e2adc6d3b8a7deb66605a115d63d39ebd1f420bd006bc1131e260fad1caaf1c6',
+    URI: 'postgres://jdtnzratgdzskw:e2adc6d3b8a7deb66605a115d63d39ebd1f420bd006bc1131e260fad1caaf1c6@ec2-54-224-124-241.compute-1.amazonaws.com:5432/d44arciics89r9',
+    HerokuCLI: 'heroku pg:psql postgresql-adjacent-05588 --app rest-api-harrisson'
+    
   },
   pool: {
     min: 2,
