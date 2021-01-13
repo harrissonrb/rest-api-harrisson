f9968dcea5c20a925e644351320c0bfa5e98ab08
harrissonrb <harrissonrocha@hotmail.com>
2021-01-12 21:06:29 -0300

    Adicionando Procfile

--- backend/config/middlewares.js
+++
@@ -1,9 +0,0 @@
-const bodyParses = require('body-parser')
-const cors = require('cors')
-
-module.exports = app => {
-    app.use(bodyParses.json())
-    app.use(cors({
-        origin: '*'
-    }))
-}
